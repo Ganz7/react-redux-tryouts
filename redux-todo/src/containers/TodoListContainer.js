02c8accc97daa428ab9ee348013af1720ab0bb42
Ganz7 <ganzse7en@gmail.com>
2016-08-14 17:02:48 -0700

    Removed silly addition

--- redux-todo/src/containers/TodoListContainer.js
+++ redux-todo/src/containers/TodoListContainer.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import TodoList from '../components/TodoList';
 
-const mapStateToProps = (state = []) => {
+const mapStateToProps = (state) => {
   return {
     todos: state.todos
     //todos: [{id:1, text: "HI PEOPLE"}, {id:2, text: "WHATS UP PEOPLE"}]
